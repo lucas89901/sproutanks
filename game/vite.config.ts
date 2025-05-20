ad3d39413939993b3e4b9fd1a4feb7e172cda3f7
Lucas Lu <lucas89901@gmail.com>
2025-05-20 21:24:51 +0800

    Deploy to GitHub Pages

--- game/vite.config.ts
+++ game/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
+import {defineConfig} from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sproutanks/',
   server: {
     port: 8080,
     // open: true,
